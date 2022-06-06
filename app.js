e069e456d6f92da3cf985f31e8b8fbd0ba1b2c69
KerimGurbaz <kerim.grbz.01@gmail.com>
2022-06-06 15:41:29 +0200

    finished

--- app.js
+++ app.js
@@ -4,15 +4,16 @@ const amount = document.querySelector("#amount");
 const calculate = document.querySelector(".btn");
 let rate = 0;
 let installment = 0;
+
 calculate.addEventListener("click", (e) => {
     e.preventDefault();
-    if (select.value === "personalCredit") {
+    if (select.value === "PERSONAL FINANCE CREDIT") {
         rate = 1.99
-    } else if (select.value === "autoCredit") {
+    } else if (select.value === "AUTO CREDIT") {
         rate = 1.79
-    } else if (select.value === "housingCredit") {
+    } else if (select.value === "HOUSING CREDIT") {
         rate = 1.29
-    }
+    };
 
     if (!select.value || !period.value || !amount.value) {
         alert("Please fill in the blank fields.")
@@ -22,4 +23,36 @@ calculate.addEventListener("click", (e) => {
     installment = (amount.value * (rate * (1 + interest) ** period.value)) / ((1 + interest) ** period.value - 1)
 
     console.log(installment);
+    showResult();
 });
+
+const showResult = () => {
+    const result = document.querySelector(".result");
+    result.innerHTML = `
+    <table class="table table-bordered border-warning">
+    <tbody>
+    <tr>
+      <th >Amount of Credit</th>
+      <td>${amount.value}</td>
+      <th >Credit Type</th>
+      <td>${select.value}</td>
+    </tr>
+
+    <tr>
+    <th >Period</th>
+      <td>${period.value}</td>
+      <th>Credit Rate</th>
+      <td>${rate}</td>
+    </tr>
+
+    <tr>
+    <th > Total Amount</th>
+      <td>${(installment * period.value).toFixed(2)}</td>
+      <th >Installment Amount</th>
+      <td>${installment.toFixed(2)}</td>
+    </tr>
+  </tbody>
+
+</table>
+    `
+}
